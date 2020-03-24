15ba83fbb5dd9292e451de0521d75207b2c486bb
vishkid84 <vishalittr@gmail.com>
2020-03-24 00:14:13 +0000

    Added fetch to get data from API

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,37 +1,27 @@
-// const baseURL = "http://www.omdbapi.com/?i=tt3896198&apikey=f0d1e078";
 
-// function getData(query, cb) {
-//     var xhr = new XMLHttpRequest();
-
-//     xhr.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//             cb(JSON.parse(this.responseText));
-//         }
-//     };
-
-//     xhr.open("GET", baseURL + "&" + query);
-//     xhr.send();
-// }
-
-// function writeToDocument(query) {
-//     getData(query, function(data) {
-//         console.dir(data);
-//         document.getElementById("pickMovieBtn").innerHTML = data.Title;
-//     });
-// }
 
 const url = "http://api.themoviedb.org/3/search/movie?api_key=a633b83aa763a0e8fad2c80cc66c54b9&query=kerala";
-// var movies = data.Search;
- fetch(url).then(function(response){
+
+function getMovie() {
+    fetch(url).then(function (response) {
         return response.json();
-    }).then(function(data){
-        console.log(data);
+    }).then(function (data) {
         console.dir(data);
-        document.getElementById("Title").innerHTML = data;
-    }).catch(function(error){
+        var movies = data.results;
+        $("#searchBtn").click(function () {
+            var el = document.getElementById("Title");
+            el.innerHTML = "";
+            movies.forEach(function (movie) {
+                el.innerHTML += `<p>${movie.title}</p>`;
+            });
+        })
+    }).catch(function (error) {
         console.error("Something has gone wrong");
         console.error(error);
     })
+}
+
+getMovie();
 
 // function(getMovie(){
 
